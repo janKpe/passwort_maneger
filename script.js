cdb72f0a4ead6cfd196bdd8555410483ce3aa199
janinixd <jan.kupke@web.de>
2023-11-25 18:51:19 +0100

    passwords are now saved in local storage and remove button works

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
-const passwords = [];
+let passwords = localStorage.getItem("passwords");
 
-
-function choose(choices) {
-  var index = Math.floor(Math.random() * choices.length);
-  return choices[index];
+if (!passwords) {
+  localStorage.setItem("passwords", JSON.stringify({"passwordList": []}))
+  passwords = {"passwordList": []}
+} else {
+  passwords = JSON.parse(passwords)
+  renderPasswords()
 }
 
+
+
 function generatePassword() {
-  const website = document.getElementById("website").value;
-  const username = document.getElementById("username").value;
-  const beleidigung = choose(["idiot", "mast", "sau"]);
   const password = Math.random().toString(36).slice(-10);
 
-  document.getElementById("password").value = password + beleidigung;
-
-  const passwordObject = {
-    website: website,
-    username: username,
-    password: password
-  };
-
-  passwords.push(passwordObject);
-
-  document.getElementById("passwordlList").innerHTML +="<div class='passwordItem'>"+ 
-  "<img src='assets/delete.svg' class='deleteIcon' >" +
-
-"  <div class='passwordRow'>" +
-"    <img src='assets/website.svg'>" +
-`    <p id='website'><a href="${website}">${website}</a></p>`+
-  "</div>"+
-    "<div class='passwordRow'>"+
-      "<img src='assets/user.svg' >"+
-      `<p id='username' onclick='copyToClipboard("${username}")'>${username}</p>`+
-    "</div>"+
-    "<div class='passwordRow'>"+
-      "<img src='assets/password.svg'>"+
-      `<p id='password' onclick='copyToClipboard("${password}")'>${password}</p>`+
-    "</div>"+
-"</div>"
+  document.getElementById("password").value = password;
 
   
+}
+
+function renderPasswords() {
+  document.getElementById("passwordlList").innerHTML = ""
+  passwords.passwordList.forEach(element => {
+    document.getElementById("passwordlList").innerHTML +="<div class='passwordItem'>"+ 
+    `<img src='assets/delete.svg' class='deleteIcon' onclick="deletePassword('${element.id}')">` +
+  
+  "  <div class='passwordRow'>" +
+  "    <img src='assets/website.svg'>" +
+  `    <p id='website'><a target="_blank" href="${element.website}">${element.website}</a></p>`+
+    "</div>"+
+      "<div class='passwordRow'>"+
+        "<img src='assets/user.svg' >"+
+        `<p id='username' onclick='copyToClipboard("${element.username}")'>${element.username}</p>`+
+      "</div>"+
+      "<div class='passwordRow'>"+
+        "<img src='assets/password.svg'>"+
+        `<p id='password' onclick='copyToClipboard("${element.password}")'>${element.password}</p>`+
+      "</div>"+
+  "</div>"
+  });
+}
+function savePassword() {
+  const website = document.getElementById("website").value;
+  const username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if (!password) {
+    password = Math.random().toString(36).slice(-10);
+  }
+
+  if (website && username) {
+    const id = Math.random().toString(36).slice(-10); 
+    const passwordObject = {
+      "website": website,
+      "username": username,
+      "password": password,
+      "id": id
+    };
+
+    document.getElementById("website").value = "";
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+
+
+
+    passwords["passwords"] = passwords["passwordList"].push(passwordObject);
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+    renderPasswords()
+  }
+ 
+
 }
 
 
@@ -55,15 +84,14 @@ function copyToClipboard(textToCopy) {
 }
 
 
-function savePassword() {
-    const passwordString = JSON.stringify(passwords);
-  
-    const blob = new Blob([passwordString], { type: "application/json" });
-    const link = document.createElement("a");
-    link.download = "passwords.json";
-    link.href = URL.createObjectURL(blob);
-    link.click();
-  }
-  
 
-
+function deletePassword(id) {
+  passwords.passwordList.forEach(element => {
+    if (element.id === id) {
+      var index = passwords.passwordList.indexOf(element);
+      passwords.passwordList.splice(index, 1);
+      localStorage.setItem("passwords", JSON.stringify(passwords))
+      renderPasswords()
+    }
+  })
+}
